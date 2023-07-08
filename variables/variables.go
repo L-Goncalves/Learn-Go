a5eea0e0d3b63ea4dd456916a9613103d24f25ab
Lucas Gonçalves <ldgoncalvs@gmail.com>
2023-07-08 03:38:08 -0300

    add: variables

---
+++ variables/variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string = "Lucas"
+	var version float32 = 1.1
+	var age int = 22 // Int if not defined becomes 0;
+
+	fmt.Println("Hello,", name, "Your age is", age)
+	fmt.Println("This Program is at version", version)
+}
