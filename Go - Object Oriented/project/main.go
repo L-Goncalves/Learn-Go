c713125c556f87a5023dadf182a42f28a164cae9
Lucas Gonçalves <ldgoncalvs@gmail.com>
2023-07-31 12:36:50 -0300

    feat: adding add balance method

--- Go - Object Oriented/project/main.go
+++ Go - Object Oriented/project/main.go
@@ -9,15 +9,25 @@ type BankAccount struct {
 	balance       float64
 }
 
-func (c *BankAccount) withdraw(withdrawValue float64) string {
+func (c *BankAccount) withdraw(withdrawValue float64) (string, float64) {
 	canWithdraw := withdrawValue > 0 && withdrawValue < c.balance
 
 	if canWithdraw {
 		c.balance -= withdrawValue
-		return "Withdraw Successful"
+		return "Withdraw Successful", c.balance
 	}
 
-	return "Balance Unsuficient to complete transaction"
+	return "Balance Unsuficient to complete transaction", c.balance
+}
+
+func (c *BankAccount) addBalance(balanceAdded float64) (string, float64) {
+	if balanceAdded > 0 {
+		c.balance += balanceAdded
+		return "Balance added Successfully", c.balance
+	}
+
+	return "An Error Occurred while attempting to add balance.", c.balance
+
 }
 
 func main() {
@@ -26,7 +36,7 @@ func main() {
 
 	josueAccount.balance = 700
 
-	josueAccount.withdraw(600)
+	josueAccount.withdraw(700)
 
-	fmt.Println(josueAccount.balance)
+	fmt.Println(josueAccount.addBalance(6500))
 }
